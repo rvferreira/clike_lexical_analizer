7df475b79a4452647be9513fef12530173f17223
Raphael Ferreira <raphael.rvf@gmail.com>
2015-05-27 16:05:04 -0300

    personalized errors

--- ParseException.java
+++ ParseException.java
@@ -97,11 +97,11 @@ public class ParseException extends Exception {
         expected.append(tokenImage[expectedTokenSequences[i][j]]).append(' ');
       }
       if (expectedTokenSequences[i][expectedTokenSequences[i].length - 1] != 0) {
-        expected.append("...");
+        expected.append(",");
       }
       expected.append(eol).append("    ");
     }
-    String retval = "Encountered \"";
+    String retval = "AnalSin encontrou ";
     Token tok = currentToken.next;
     for (int i = 0; i < maxSize; i++) {
       if (i != 0) retval += " ";
@@ -109,18 +109,20 @@ public class ParseException extends Exception {
         retval += tokenImage[0];
         break;
       }
-      retval += " " + tokenImage[tok.kind];
+      retval += tokenImage[tok.kind];
+      
       retval += " \"";
       retval += add_escapes(tok.image);
       retval += " \"";
+      
       tok = tok.next;
     }
-    retval += "\" at line " + currentToken.next.beginLine + ", column " + currentToken.next.beginColumn;
+    retval += " na linha " + currentToken.next.beginLine + ", coluna " + currentToken.next.beginColumn;
     retval += "." + eol;
     if (expectedTokenSequences.length == 1) {
-      retval += "Was expecting:" + eol + "    ";
+      retval += "Quando na verdade, queriamos ter encontrado:" + eol + "    ";
     } else {
-      retval += "Was expecting one of:" + eol + "    ";
+      retval += "Quando na verdade, queriamos ter encontrado um desses:" + eol + "    ";
     }
     retval += expected.toString();
     return retval;
