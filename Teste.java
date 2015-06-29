5b3aa11b2f6e061cf2021af55ac4acc02b31589b
Raphael Ferreira <raphael.rvf@gmail.com>
2015-06-28 22:57:29 -0300

    insert remove bugfix

--- Teste.java
+++ Teste.java
@@ -1,35 +1,21 @@
-
-
 //ident, nivel, categoria, tipo, valor, nbytes, ender, classe_transf, npar, tamanho
 public class Teste
 { 	
 
-
-
 	public static void main(String[] args)
 	{
 		TS tabela = new TS();
-
+		
 		Atributos atributos = new Atributos ("int",0,Categoria.TIPO,CampoTipo.INT,"0",4,0,0,0,0);
 		Simbolo simbolo = SimboloFactory.getInstance(atributos);
 		tabela.Insere(simbolo);
-
-		System.out.println(tabela.nivel);
 
 		Atributos atributos2 = new Atributos ("int",3,Categoria.TIPO,CampoTipo.INT,"0",4,0,0,0,0);
 		Simbolo simbolo2 = SimboloFactory.getInstance(atributos2);
 		tabela.Insere(simbolo2);
 
-		System.out.println(tabela.nivel);
-
 		Atributos atributos3 = new Atributos ("float",0,Categoria.TIPO,CampoTipo.INT,"0",4,0,0,0,0);
 		Simbolo simbolo3 = SimboloFactory.getInstance(atributos3);
 		tabela.Insere(simbolo3);
-
-		System.out.println(tabela.nivel);
-
 	}
-
-
-
 }
