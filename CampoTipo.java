d92aff1ec2938b5a99a176187da650fdb93fd2af
laispc <laispc19@gmail.com>
2015-06-28 21:25:40 -0300

    New files, teste

---
+++ CampoTipo.java
@@ -0,0 +1,8 @@
+public class CampoTipo
+{
+	
+	public static final int INT = 0;
+	public static final int FLOAT = 1;
+	public static final int CHAR = 2;
+
+}
---
+++ Categoria.java
@@ -0,0 +1,10 @@
+public class Categoria
+{
+	
+	public static final int TIPO = 0;
+	public static final int CONSTANTE = 1;
+	public static final int PROCEDIMENTO = 2;
+	public static final int FUNCAO = 3;
+	public static final int PARAMETRO = 4;
+	public static final int VARIAVEL = 5;
+}
